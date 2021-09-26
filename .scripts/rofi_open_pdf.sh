b72562f07e3ab9b8ff0fa5d75ad04cd9885fa5b8
Sergey Pikhota <sergey.pikhota@protonmail.com>
2021-09-26 23:44:37 +0200

    Make pdf search case-insensitive

--- .scripts/rofi_open_pdf.sh
+++ .scripts/rofi_open_pdf.sh
@@ -3,4 +3,4 @@
 [ -e ~/.local/share/pdfDirs ] || exit 1
 
 args=$(awk '{s=s " --search-path "$1" "} END {print s}' ~/.local/share/pdfDirs)
-fd -e pdf $args -d 1 | rofi -dmenu | xargs -I _ -r zathura "_"
+fd -e pdf $args -d 1 | rofi -dmenu -i | xargs -I _ -r zathura "_"
