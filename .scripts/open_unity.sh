465d384a0720e4c3185e3b080968a50a9befa7db
Sergey Pikhota <sergey.pikhota@protonmail.com>
2022-01-16 23:00:20 +0100

    Improve open unity script

--- .scripts/open_unity.sh
+++ .scripts/open_unity.sh
@@ -3,6 +3,6 @@
 UNITY_VERSION="2020.3.25f1"
 UNITY_PATH=~/Unity/Hub/Editor/$UNITY_VERSION/Editor/Unity
 
-selected_project=$(./get_projects.sh | rofi -dmenu)
+selected_project=$(~/.scripts/get_projects.sh | rofi -dmenu -i)
 
-$UNITY_PATH -projectPath $selected_project
+[ -d $selected_project ] && $UNITY_PATH -projectPath $selected_project
