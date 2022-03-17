96119602f4f552d5d864002967286c27ae1579f5
Melesar <sergey.pikhota@protonmail.com>
2022-03-17 13:55:13 +0100

    Adjust pass script

--- .scripts/pass.sh
+++ .scripts/pass.sh
@@ -3,4 +3,4 @@
 DIR=~/.password-store
 [ ! -d $DIR ] && exit;
 
-fd . --base-directory $DIR -t file | sed 's/.gpg//' | rofi -dmenu | xargs pass -c
+fd . --base-directory $DIR -t file | sed 's/.gpg//' | rofi -dmenu -i -p "Choose password" | xargs pass -c
